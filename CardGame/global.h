f4c62b48a4186f97231cc65cb9384a9e45b8344b
ryan0Z2l <136170882+ryan0Z2l@users.noreply.github.com>
2025-10-18 12:40:48 +0800

    Update global.h

--- CardGame/global.h
+++ CardGame/global.h
@@ -21,14 +21,12 @@ extern int TowerRadius[];
 extern char TowerClass[][20];
 extern char SheepClass[][10];
 extern char FunctionClass[][20];
-extern int TowerWidth[];
-extern int TowerHeight[];
+
 
 /*TODO:*/
 // Declare key_state
 extern bool key_state[ALLEGRO_KEY_MAX];
 
-enum {ARCANE = 0, ARCHER, CANON, POISON, STORM};
-enum {WOLF = 0, WOLFKNIGHT, DEMONNIJIA, CAVEMAN};
+
 
 #endif // GLOBAL_H_INCLUDED
