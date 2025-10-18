90870cd8ec0922d4629f4e83dc17a011eac4f964
ryan0Z2l <136170882+ryan0Z2l@users.noreply.github.com>
2025-10-18 12:50:39 +0800

    Update DataCenter.h

--- CardGame/DataCenter.h
+++ CardGame/DataCenter.h
@@ -2,26 +2,12 @@
 #define DATACENTER_H_INCLUDED
 #include <vector>
 #include <list>
-#include "Monster.h"
-#include "Tower.h"
-#include "Hero.h"
 #include "SheepCard.h"
 #include "Card.h"
 
 class DataCenter{
 public:
-    std::vector<Monster*>& get_Monster()
-    {
-        return monsterSet;
-    }
-    std::list<Tower*>& get_Tower()
-    {
-        return towerSet;
-    }
-    Hero& get_Hero()
-    {
-        return dragonite;
-    }
+    
     Deck& get_Deck()
     {
         return deck;
@@ -31,10 +17,8 @@ public:
         return  sheep;
     }
 private:
-    std::vector<Monster*> monsterSet;
-    std::list<Tower*> towerSet;
+
     std::list<SheepCard*> sheepSet;
-    Hero dragonite;
     Deck deck;
     SheepCard sheep;
 };
